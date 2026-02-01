5d03c8b25d23a62fe55c9678f9b4bafa897198f8
thatsnotrlght <jartiste23@gmail.com>
2026-02-01 14:35:53 -0500

    Initial commit

---
+++ src/main/java/com/thatsnotrlght/interview_radar/InterviewRadarApplication.java
@@ -0,0 +1,13 @@
+package com.thatsnotrlght.interview_radar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewRadarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterviewRadarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thatsnotrlght/interview_radar/InterviewRadarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thatsnotrlght.interview_radar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewRadarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
